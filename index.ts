dfa68adbc0faa24b6d574a1316cb4e59050347b6
Ayomide24500 <Ayomideadisa83@gmail.com>
2024-03-03 23:17:43 +0100

    dev

--- index.ts
+++ index.ts
@@ -6,25 +6,27 @@ import { mainApp } from "./mainApp";
 // import helmet from "helmet";
 import { dbConfig } from "./utils/dbConfig";
 
-// import MongoDB from "connect-mongodb-session";
+import MongoDB from "connect-mongodb-session";
+import dotenv from "dotenv";
+dotenv.config();
 
-// const MongoDBStore = MongoDB(session);
-// const store = new MongoDBStore({
-//   uri: process.env.MONGODB_URL_ONLINE!,
-//   collection: "sessions",
-// });
+const MongoDBStore = MongoDB(session);
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URL_ONLINE!,
+  collection: "sessions",
+});
 
 const port: number | string = process.env.port || 1200;
 
 const app: Application = express();
 
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//   res.header("Access-Control-Allow-Origin", process.env.APP_URL_DEPLOY);
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", process.env.APP_URL_DEPLOY);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 // app.use(helmet());
 // app.use(morgan("dev"));
@@ -32,29 +34,29 @@ const app: Application = express();
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET!,
-//     resave: false,
-//     saveUninitialized: false,
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
 
-//     cookie: {
-//       maxAge: 1000 * 60 * 24 * 60,
-//       // sameSite: "lax",
-//       // secure: false,
-//       httpOnly: true,
-//       // domain: process.env.APP_URL_DEPLOY,
-//     },
+    cookie: {
+      maxAge: 1000 * 60 * 24 * 60,
+      sameSite: "lax",
+      secure: false,
+      httpOnly: true,
+      domain: process.env.APP_URL_DEPLOY,
+    },
 
-//     // store,
-//   })
-// );
+    store,
+  })
+);
 
 mainApp(app);
 const server = app.listen(port, async () => {
   console.clear();
   console.log("connected 👌👍");
-  await dbConfig();
+  dbConfig();
 });
 
 process.on("uncaughtException", (err: Error) => {
--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -2,15 +2,12 @@ import { connect } from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const URL = "mongodb://localhost:27017/pro";
-
 export const dbConfig = async () => {
   try {
-    return await connect(process.env.MONGODB_URL_ONLINE!).then(() => {
-      console.log("database connection established🔥❤️🔥");
-    });
+    await connect(process.env.MONGODB_URL_ONLINE!);
+    console.log("Database connection established🔥❤️🔥");
   } catch (error) {
-    console.error("Error connecting to database:", error);
-    return error;
+    console.log("Error connecting to database:", error);
+    throw error;
   }
 };
