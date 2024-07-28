2e873ced1d6c2722d60eaea330951cb3ba036276
Ayomide24500 <142061740+Ayomide24500@users.noreply.github.com>
2024-07-28 20:08:53 +0100

    Update index

--- index.ts
+++ index.ts
@@ -29,9 +29,9 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
-  app.use(cors({ origin: allowedOrigins, credentials: true }));
 });
 
+app.use(cors({ origin: "https://project-mastros-justyou.vercel.app" }));
 app.use(express.json());
 
 app.use(
